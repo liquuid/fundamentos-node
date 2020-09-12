57f1f81ab0e866d8f32c01c088f6a38332019fc4
Fernando Henrique <liquuid@gmail.com>
2020-09-12 18:05:38 -0300

    finished

---
+++ dist/services/CreateTransactionService.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CreateTransactionService = /** @class */ (function () {
+    function CreateTransactionService(transactionsRepository) {
+        this.transactionsRepository = transactionsRepository;
+    }
+    CreateTransactionService.prototype.execute = function (_a) {
+        var title = _a.title, type = _a.type, value = _a.value;
+        var transaction = this.transactionsRepository.create({
+            title: title,
+            type: type,
+            value: value,
+        });
+        return transaction;
+    };
+    return CreateTransactionService;
+}());
+exports.default = CreateTransactionService;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface TransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: TransactionDTO): Transaction {
+    if (['income', 'outcome'].includes(type)) {
+      throw Error('Transaction type invalid');
+    }
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && total < value) {
+      throw Error('Insuficient fund');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    return transaction;
   }
 }
 
